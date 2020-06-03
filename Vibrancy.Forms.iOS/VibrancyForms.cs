c0c2cceac766368036d3a7331187eb7baf46dcf0
Andreas Wäscher <awaescher@sodacore.net>
2020-06-04 00:06:57 +0200

    Add a sample app

---
+++ Vibrancy.Forms.iOS/VibrancyForms.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Vibrancy.Forms.iOS
+{
+	public static class VibrancyForms
+	{
+		public static void Init()
+		{
+			// nothing to do, just let the platform projects
+			// call Init() to prevent the project reference
+			// to be optimized away.
+		}
+	}
+}
